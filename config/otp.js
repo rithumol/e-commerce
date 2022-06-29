0e810a0f8080e6d9bfcc1dcef141222d0ca9fd12
rithumol <rithumoltr@gmail.com>
2022-06-29 13:35:40 +0530

    otp-login update

--- config/otp.js
+++ config/otp.js
@@ -1,5 +1,5 @@
 module.exports={
-    serviceID:"VA77c0bd744b88f03f4437df1dbcfc2777",
-    accountSID:"ACd759bad186dbc1ec90aab19791c77b7f",
-    authToken:"fe41f47c304c07c9f83a4b8d2daca0c0"
+    serviceID: process.env.SERVICE_ID,
+    accountSID: process.env.ACCOUNT_SID,
+    authToken: process.env.AUTH_TOKEN
 }
